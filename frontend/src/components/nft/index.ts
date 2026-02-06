5afec4f2a686b2b0a4c201e17127f8fd4884ac64
Gbolahan Akande <geakande@gmail.com>
2026-02-06 23:52:39 +0100

    Export NFT components from index

---
+++ frontend/src/components/nft/index.ts
@@ -0,0 +1,8 @@
+/**
+ * NFT Components Export Index
+ */
+
+export { CreativeMintingUI } from './CreativeMintingUI';
+export { MarketplaceGallery } from './MarketplaceGallery';
+export { ImpressionShowcase } from './ImpressionShowcase';
+export { RoyaltyCalculator } from './RoyaltyCalculator';
