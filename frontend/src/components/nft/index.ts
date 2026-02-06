ed86a76ccbfa45316a2bd0426930181b316bb81d
Gbolahan Akande <geakande@gmail.com>
2026-02-06 23:59:34 +0100

    Update NFT component exports index

--- frontend/src/components/nft/index.ts
+++ frontend/src/components/nft/index.ts
@@ -6,3 +6,7 @@ export { CreativeMintingUI } from './CreativeMintingUI';
 export { MarketplaceGallery } from './MarketplaceGallery';
 export { ImpressionShowcase } from './ImpressionShowcase';
 export { RoyaltyCalculator } from './RoyaltyCalculator';
+export { NFTMetadataEditor } from './NFTMetadataEditor';
+export { CreativeLicenseManager } from './CreativeLicenseManager';
+export { NFTTransferInterface } from './NFTTransferInterface';
+export { NFTCollectionViewer } from './NFTCollectionViewer';
