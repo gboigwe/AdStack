b19f0cbdb89ab2b8aefa69a3d89781c5f136a79a
Gbolahan Akande <geakande@gmail.com>
2026-02-08 14:22:22 +0100

    Add token selector and balance viewer

--- frontend/src/components/bridge/index.ts
+++ frontend/src/components/bridge/index.ts
@@ -1,2 +1,4 @@
 export { default as BridgeInterface } from './BridgeInterface';
 export { default as TransactionHistory } from './TransactionHistory';
+export { default as TokenSelector } from './TokenSelector';
+export { default as BalanceViewer } from './BalanceViewer';
