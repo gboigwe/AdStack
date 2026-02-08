1cc96ef1dc2d1c6dade9c9043a1a2b6a9cfc6647
Gbolahan Akande <geakande@gmail.com>
2026-02-08 14:23:37 +0100

    Add multi-token payment dashboard

--- frontend/src/components/bridge/index.ts
+++ frontend/src/components/bridge/index.ts
@@ -2,3 +2,4 @@ export { default as BridgeInterface } from './BridgeInterface';
 export { default as TransactionHistory } from './TransactionHistory';
 export { default as TokenSelector } from './TokenSelector';
 export { default as BalanceViewer } from './BalanceViewer';
+export { default as PaymentDashboard } from './PaymentDashboard';
