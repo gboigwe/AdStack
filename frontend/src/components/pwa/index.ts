4c980ad42382eeab6c03d1353f7d343fe8c4ceed
0xMosas <tosinolosasa@gmail.com>
2026-02-27 15:51:20 +0100

    feat: add barrel exports for PWA module

---
+++ frontend/src/components/pwa/index.ts
@@ -0,0 +1,8 @@
+export { OfflineBanner, OfflineCard, OfflineGate } from './OfflineFallback';
+export { NotificationPrompt, NotificationToggle } from './NotificationPrompt';
+export { NotificationCenter } from './NotificationCenter';
+export { InstallPrompt } from './InstallPrompt';
+export { UpdateNotification } from './UpdateNotification';
+export { LazyComponent, LazyImage, prefetchRoute, preconnect } from './LazyLoad';
+export { PWAProvider, usePWAContext } from './PWAProvider';
+export { PWAStatusDashboard } from './PWAStatusDashboard';
