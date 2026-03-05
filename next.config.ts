70c30d88810147a37f99e175cecec5a25670d64e
Gbolahan Akande <geakande@gmail.com>
2026-03-05 09:42:35 +0100

    Add transpilePackages for Stacks libs to fix Turbopack SSR crash

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: [
+    '@stacks/connect',
+    '@stacks/network',
+    '@stacks/transactions',
+    '@stacks/common',
+    '@stacks/blockchain-api-client',
+  ],
 };
 
 export default nextConfig;
