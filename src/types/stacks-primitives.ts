2ce998565d1aed9741332b7c6d18fd7037f9fa1d
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:08:00 +0100

    add StxAddress ContractAddress ContractName branded types

--- src/types/stacks-primitives.ts
+++ src/types/stacks-primitives.ts
@@ -8,3 +8,7 @@ export type Int128 = Brand<bigint, 'Int128'>;
 export type TxId = Brand<string, 'TxId'>;
 export type BlockHash = Brand<string, 'BlockHash'>;
 export type BurnBlockHash = Brand<string, 'BurnBlockHash'>;
+
+export type StxAddress = Brand<string, 'StxAddress'>;
+export type ContractAddress = Brand<string, 'ContractAddress'>;
+export type ContractName = Brand<string, 'ContractName'>;
