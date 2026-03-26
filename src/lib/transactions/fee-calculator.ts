00773113a725ec65e5ab2cc484509320bcb9589d
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:00:00 +0100

    add export type FeeEstimate = { low: bigint; medium: bigint; high: bigint } to fee-calculator

--- src/lib/transactions/fee-calculator.ts
+++ src/lib/transactions/fee-calculator.ts
@@ -5,3 +5,5 @@ export const MIN_FEE = BigInt(180);
 export const DEFAULT_FEE = BigInt(1000);
 
 export const FEE_RATE = BigInt(180);
+
+export type FeeEstimate = { low: bigint; medium: bigint; high: bigint };
