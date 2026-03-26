250bcb3e5c7f5f77631dc098bec49c31d9148f69
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:16:00 +0100

    define SomeCV generic wire type

--- src/lib/stacksjs/clarity-value-factory.ts
+++ src/lib/stacksjs/clarity-value-factory.ts
@@ -6,3 +6,4 @@ export type UintCV = { type: 'uint'; value: bigint };
 export type IntCV = { type: 'int'; value: bigint };
 export type BoolCV = { type: 'bool'; value: boolean };
 export type NoneCV = { type: 'none' };
+export type SomeCV<T> = { type: 'some'; value: T };
