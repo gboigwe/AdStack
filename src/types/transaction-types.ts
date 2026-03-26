1501a9cce5cbb7c2b68b8448aa4104a6e63a623b
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:26:00 +0100

    add TransactionType union

--- src/types/transaction-types.ts
+++ src/types/transaction-types.ts
@@ -1,2 +1,9 @@
 // Typed transaction structures for Stacks
 import type { TxId, StxAddress, BlockHeight, MicroStx, Nonce, HexString } from './stacks-primitives';
+
+export type TransactionType =
+  | 'token_transfer'
+  | 'contract_call'
+  | 'smart_contract'
+  | 'coinbase'
+  | 'poison_microblock';
