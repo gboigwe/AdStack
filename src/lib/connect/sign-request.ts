575d352c8a16140e70c4b9d35b0b10d3df092b74
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:40:00 +0100

    add export type StructuredDataDomain = { nam to sign-request

--- src/lib/connect/sign-request.ts
+++ src/lib/connect/sign-request.ts
@@ -3,3 +3,5 @@
 export type SignMessageParams = { message: string; onFinish?: (data: SignatureData) => void; onCancel?: () => void };
 
 export type SignatureData = { signature: string; publicKey: string };
+
+export type StructuredDataDomain = { name: string; version: string; chainId: number };
