fae265101b28448d2609f66f4ef75fb3f667107d
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:40:00 +0100

    add export type ContractCallRequestParams =  to contract-call-request

--- src/lib/connect/contract-call-request.ts
+++ src/lib/connect/contract-call-request.ts
@@ -5,3 +5,5 @@ export type Network = 'mainnet' | 'testnet' | 'devnet';
 export type PostConditionMode = 'allow' | 'deny';
 
 export type AnchorMode = 'any' | 'on_chain_only' | 'off_chain_only';
+
+export type ContractCallRequestParams = { contractAddress: string; contractName: string; functionName: string; functionArgs: unknown[]; network: Network; postConditions?: unknown[]; postConditionMode?: PostConditionMode; anchorMode?: AnchorMode; fee?: bigint; nonce?: bigint; onFinish?: (data: TxBroadcastResult) => void; onCancel?: () => void };
