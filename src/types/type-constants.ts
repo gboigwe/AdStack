2f727081e6c7b4e02d68c7884a44121f000ae4ed
Gbolahan Akande <geakande@gmail.com>
2026-03-26 18:20:00 +0100

    add TYPE_GUARD_ID_221 constant

--- src/types/type-constants.ts
+++ src/types/type-constants.ts
@@ -220,3 +220,4 @@ export const TYPE_GUARD_ID_217 = 217;
 export const TYPE_GUARD_ID_218 = 218;
 export const TYPE_GUARD_ID_219 = 219;
 export const TYPE_GUARD_ID_220 = 220;
+export const TYPE_GUARD_ID_221 = 221;
