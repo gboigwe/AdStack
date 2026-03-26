9d56efb78f3c9daa47837001cd9408ca93d8795d
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:28:00 +0100

    add export type NftHoldingsResponse = { limit: number; offset: number; total: number; results: NftInfo[] } to tokens-api

--- src/lib/api/tokens-api.ts
+++ src/lib/api/tokens-api.ts
@@ -4,3 +4,5 @@ import { HIRO_API_BASE, HIRO_TESTNET_BASE, Network } from './accounts-api';
 export type FtInfo = { name: string; symbol: string; decimals: number; total_supply: string; token_uri: string; contract_id: string };
 
 export type NftInfo = { asset_identifier: string; value: { hex: string; repr: string }; block_height: number; tx_id: string };
+
+export type NftHoldingsResponse = { limit: number; offset: number; total: number; results: NftInfo[] };
