2eb5916832cd7aec265bbce8446af15185464326
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:04:00 +0100

    add export type AppDetails = { name: string; to auth-request

--- src/lib/connect/auth-request.ts
+++ src/lib/connect/auth-request.ts
@@ -1,3 +1,5 @@
 // Stacks Connect auth request builders
 
 export type AuthRequestParams = { appDetails: AppDetails; redirectTo?: string; onFinish?: (payload: AuthResponsePayload) => void; onCancel?: () => void };
+
+export type AppDetails = { name: string; icon: string };
