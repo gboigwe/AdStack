88f982203e5cac2bf60ae852fdd25c50847db8e9
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:26:00 +0100

    add isMainnetAddress and isTestnetAddress

--- src/utils/address-utils.ts
+++ src/utils/address-utils.ts
@@ -2,3 +2,10 @@
 
 const MAINNET_PREFIX = 'SP';
 const TESTNET_PREFIX = 'ST';
+
+export function isMainnetAddress(address: string): boolean {
+  return address.startsWith(MAINNET_PREFIX);
+}
+export function isTestnetAddress(address: string): boolean {
+  return address.startsWith(TESTNET_PREFIX);
+}
