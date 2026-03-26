350ad84c64ed3ef17676e7e67e6371e286cc966b
Gbolahan Akande <geakande@gmail.com>
2026-03-26 12:44:00 +0100

    define ClarityString union type

--- src/lib/clarity-v4/clarity-strings.ts
+++ src/lib/clarity-v4/clarity-strings.ts
@@ -3,3 +3,5 @@
 export type StringAscii = { type: 'string-ascii'; value: string; maxLength: number };
 
 export type StringUtf8 = { type: 'string-utf8'; value: string; maxLength: number };
+
+export type ClarityString = StringAscii | StringUtf8;
