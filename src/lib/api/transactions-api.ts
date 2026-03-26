f56c1b632ee80d1e31f383b0044ee5739ac03177
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:38:00 +0100

    add export type FeeEstimateResponse = { estimated_cost: { read_count: number; read_length: number; runtime: number; write_count: number; write_length: number }; estimated_cost_scalar: number; estimations: FeeEstimation[] } to transactions-api

--- src/lib/api/transactions-api.ts
+++ src/lib/api/transactions-api.ts
@@ -6,3 +6,5 @@ export type Transaction = { tx_id: string; tx_type: string; tx_status: string; b
 export type MempoolTransaction = { tx_id: string; tx_type: string; receipt_time: number; sender_address: string; fee_rate: string };
 
 export type BroadcastResponse = { txid: string } | { error: string; reason: string };
+
+export type FeeEstimateResponse = { estimated_cost: { read_count: number; read_length: number; runtime: number; write_count: number; write_length: number }; estimated_cost_scalar: number; estimations: FeeEstimation[] };
