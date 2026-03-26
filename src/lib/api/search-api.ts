037d93060496743461128ebc085641cdcad3533b
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:42:00 +0100

    add export type SearchResult = { found: boolean; result: { entity_type: SearchResultType; entity_id: string } | null } to search-api

--- src/lib/api/search-api.ts
+++ src/lib/api/search-api.ts
@@ -2,3 +2,5 @@
 import { HIRO_API_BASE, HIRO_TESTNET_BASE, Network } from './accounts-api';
 
 export type SearchResultType = 'tx' | 'block' | 'contract_address' | 'standard_address';
+
+export type SearchResult = { found: boolean; result: { entity_type: SearchResultType; entity_id: string } | null };
