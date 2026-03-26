75b592f32ca4e156921dac4063d02817cba02215
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:10:00 +0100

    define IntCV wire type

--- src/lib/stacksjs/clarity-value-factory.ts
+++ src/lib/stacksjs/clarity-value-factory.ts
@@ -3,3 +3,4 @@
 export type CvType = 'uint' | 'int' | 'bool' | 'principal' | 'buffer' | 'string-ascii' | 'string-utf8' | 'list' | 'tuple' | 'optional' | 'response-ok' | 'response-err' | 'none' | 'some';
 
 export type UintCV = { type: 'uint'; value: bigint };
+export type IntCV = { type: 'int'; value: bigint };
