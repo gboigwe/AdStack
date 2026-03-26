7e973cedf691c9866cc8864715b9f0e2ba461bab
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:38:00 +0100

    add ESCROW_CONTRACT constant

--- src/examples/claim-payout.ts
+++ src/examples/claim-payout.ts
@@ -2,3 +2,5 @@
 import type { ContractWriteParams } from '../hooks/useContractWrite';
 import { makeUint } from '../lib/clarity-v4/clarity-primitives';
 import { makeStxEqCondition } from '../lib/transactions/post-condition-builder';
+
+export const ESCROW_CONTRACT = 'SP2J6ZY48GV1EZ5V2V5RB9MP66SW86PYKKNRV9EJ7.adstack-escrow';
