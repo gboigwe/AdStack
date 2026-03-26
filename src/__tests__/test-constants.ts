ee0c8212a140dd1de7efb42d1a14f117a40b3e0b
Gbolahan Akande <geakande@gmail.com>
2026-03-26 13:48:00 +0100

    add TEST_CASE_ID_106 constant

--- src/__tests__/test-constants.ts
+++ src/__tests__/test-constants.ts
@@ -107,3 +107,4 @@ export const TEST_CASE_ID_102 = 102;
 export const TEST_CASE_ID_103 = 103;
 export const TEST_CASE_ID_104 = 104;
 export const TEST_CASE_ID_105 = 105;
+export const TEST_CASE_ID_106 = 106;
