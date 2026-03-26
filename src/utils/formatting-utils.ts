ceaa15d8cfa872215ab768cde323297367a1badb
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:06:00 +0100

    add stxToUstx parser

--- src/utils/formatting-utils.ts
+++ src/utils/formatting-utils.ts
@@ -6,3 +6,9 @@ export function ustxToStx(ustx: bigint): string {
   const frac = ustx % USTX_PER_STX;
   return `${whole}.${frac.toString().padStart(6, '0')}`;
 }
+
+export function stxToUstx(stx: string): bigint {
+  const [whole, frac = '0'] = stx.split('.');
+  const fracPadded = frac.padEnd(6, '0').slice(0, 6);
+  return BigInt(whole) * USTX_PER_STX + BigInt(fracPadded);
+}
