501d0df7c6797d2e8925e2d818654f92f5fc3c54
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:40:00 +0100

    add export type SearchResultType = 'tx' | 'block' | 'contract_address' | 'standard_address' to search-api

--- src/lib/api/search-api.ts
+++ src/lib/api/search-api.ts
@@ -1,2 +1,4 @@
 // Hiro API search endpoints
 import { HIRO_API_BASE, HIRO_TESTNET_BASE, Network } from './accounts-api';
+
+export type SearchResultType = 'tx' | 'block' | 'contract_address' | 'standard_address';
