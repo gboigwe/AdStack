df6b435cf0b91dc45c5bca7c407ba90297929e32
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:24:00 +0100

    add contract address constants

--- src/examples/create-campaign.ts
+++ src/examples/create-campaign.ts
@@ -2,3 +2,6 @@
 import type { ContractWriteParams } from '../hooks/useContractWrite';
 import { makeUint } from '../lib/clarity-v4/clarity-primitives';
 import { makeStringAscii } from '../lib/clarity-v4/clarity-strings';
+
+export const ADSTACK_CONTRACT_ADDRESS = 'SP2J6ZY48GV1EZ5V2V5RB9MP66SW86PYKKNRV9EJ7';
+export const ADSTACK_CONTRACT_NAME = 'adstack-core';
