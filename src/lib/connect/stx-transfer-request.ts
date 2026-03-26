bbd8ed1198ea5ba8031df861ac19f3576efd252b
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:02:00 +0100

    add export type StxTransferParams = { recipi to stx-transfer-request

--- src/lib/connect/stx-transfer-request.ts
+++ src/lib/connect/stx-transfer-request.ts
@@ -1,1 +1,3 @@
 // Stacks Connect STX transfer request builders
+
+export type StxTransferParams = { recipient: string; amount: bigint; memo?: string; network: 'mainnet' | 'testnet'; fee?: bigint; nonce?: bigint; onFinish?: (data: unknown) => void; onCancel?: () => void };
