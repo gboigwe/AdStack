a0ba71ca2354d9f110471e7628a3bbbc1ea27a9a
Gbolahan Akande <geakande@gmail.com>
2026-03-26 18:54:00 +0100

    add SIGN_REQUEST_VERSION_11 constant

--- src/lib/connect/sign-request.ts
+++ src/lib/connect/sign-request.ts
@@ -58,3 +58,5 @@ export const SIGN_REQUEST_VERSION_8 = '8.0';
 export const SIGN_REQUEST_VERSION_9 = '9.0';
 
 export const SIGN_REQUEST_VERSION_10 = '10.0';
+
+export const SIGN_REQUEST_VERSION_11 = '11.0';
