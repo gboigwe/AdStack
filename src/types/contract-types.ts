cb3c33c4dc686bb5b2367b5de77a2a1d60cf7f5d
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:44:00 +0100

    add ClarityTypeString template literal type

--- src/types/contract-types.ts
+++ src/types/contract-types.ts
@@ -1,2 +1,16 @@
 // Typed Clarity contract and ABI structures
 import type { StxAddress } from './stacks-primitives';
+
+export type ClarityTypeString =
+  | 'uint128'
+  | 'int128'
+  | 'bool'
+  | 'principal'
+  | 'none'
+  | `(optional ${string})`
+  | `(response ${string} ${string})`
+  | `(list ${string} ${string})`
+  | `(tuple ${string})`
+  | `(string-ascii ${string})`
+  | `(string-utf8 ${string})`
+  | `(buff ${string})`;
