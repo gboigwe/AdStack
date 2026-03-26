b5284d9efa61c72dd57d633227073d5538d59544
Gbolahan Akande <geakande@gmail.com>
2026-03-26 13:48:00 +0100

    implement getDataVar value accessor

--- src/lib/clarity-v4/clarity-data-vars.ts
+++ src/lib/clarity-v4/clarity-data-vars.ts
@@ -7,3 +7,7 @@ export type DataVarUpdate<T> = { varName: string; newValue: T };
 export function makeDataVar<T>(name: string, initialValue: T): DataVar<T> {
   return { name, value: initialValue, type: 'data-var' };
 }
+
+export function getDataVar<T>(v: DataVar<T>): T {
+  return v.value;
+}
