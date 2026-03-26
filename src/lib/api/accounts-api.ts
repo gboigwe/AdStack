e12c211fad5823d3992988211be069d928ba9b8e
Gbolahan Akande <geakande@gmail.com>
2026-03-26 14:28:00 +0100

    add API_ENDPOINT_VERSION_110 version constant

--- src/lib/api/accounts-api.ts
+++ src/lib/api/accounts-api.ts
@@ -266,3 +266,5 @@ export const API_ENDPOINT_VERSION_107 = 'v107';
 export const API_ENDPOINT_VERSION_108 = 'v108';
 
 export const API_ENDPOINT_VERSION_109 = 'v109';
+
+export const API_ENDPOINT_VERSION_110 = 'v110';
