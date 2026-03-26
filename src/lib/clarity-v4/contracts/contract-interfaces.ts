fe3f0909cfd2dfcd18d5aa5adb47ee3fb9880b0a
Gbolahan Akande <geakande@gmail.com>
2026-03-26 12:02:00 +0100

    define ContractFunction descriptor type

--- src/lib/clarity-v4/contracts/contract-interfaces.ts
+++ src/lib/clarity-v4/contracts/contract-interfaces.ts
@@ -5,3 +5,10 @@ export type FunctionArg = { name: string; type: string };
 export type FunctionOutput = { type: string };
 
 export type FunctionAccess = 'public' | 'read_only' | 'private';
+
+export type ContractFunction = {
+  name: string;
+  access: FunctionAccess;
+  args: FunctionArg[];
+  outputs: FunctionOutput;
+};
