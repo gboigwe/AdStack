9943c568df3d3300704fe10909456b6262334022
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:42:00 +0100

    add FeeEstimate interface

--- src/hooks/useFeeEstimate.ts
+++ src/hooks/useFeeEstimate.ts
@@ -1,2 +1,8 @@
 // useFeeEstimate: estimate transaction fee via Hiro API
 import { useState, useCallback } from 'react';
+
+export interface FeeEstimate {
+  low: bigint;
+  medium: bigint;
+  high: bigint;
+}
