32e0ed749a8513c10555c79f2550b1792e517d9f
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:04:00 +0100

    add Uint128 and Int128 branded types

--- src/types/stacks-primitives.ts
+++ src/types/stacks-primitives.ts
@@ -1,3 +1,6 @@
 // Branded primitives for Stacks types
 declare const __brand: unique symbol;
 export type Brand<T, B> = T & { [__brand]: B };
+
+export type Uint128 = Brand<bigint, 'Uint128'>;
+export type Int128 = Brand<bigint, 'Int128'>;
