154a407d5dacd2e00c2b7c96c4088f19fbe3261d
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:08:00 +0100

    add export type AccountBalance = { stx: StxBalanc to accounts-api

--- src/lib/api/accounts-api.ts
+++ src/lib/api/accounts-api.ts
@@ -5,3 +5,5 @@ export const HIRO_API_BASE = 'https://api.hiro.so';
 export const HIRO_TESTNET_BASE = 'https://api.testnet.hiro.so';
 
 export type Network = 'mainnet' | 'testnet';
+
+export type AccountBalance = { stx: StxBalance; fungible_tokens: Record<string, FtBalance>; non_fungible_tokens: Record<string, NftHolding> };
