50355a252a6622069527d4ce4d54941b8e921faa
Gbolahan Akande <geakande@gmail.com>
2026-03-26 12:42:00 +0100

    add TYPE_GUARD_ID_52 constant

--- src/types/type-constants.ts
+++ src/types/type-constants.ts
@@ -51,3 +51,4 @@ export const TYPE_GUARD_ID_48 = 48;
 export const TYPE_GUARD_ID_49 = 49;
 export const TYPE_GUARD_ID_50 = 50;
 export const TYPE_GUARD_ID_51 = 51;
+export const TYPE_GUARD_ID_52 = 52;
