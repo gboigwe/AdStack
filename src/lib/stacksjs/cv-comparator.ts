4832d6502d581c19110f477396c346143b380a96
Gbolahan Akande <geakande@gmail.com>
2026-03-26 11:18:00 +0100

    implement cvCompare ordering comparator

--- src/lib/stacksjs/cv-comparator.ts
+++ src/lib/stacksjs/cv-comparator.ts
@@ -5,3 +5,11 @@ export function cvEqual(a: ClarityValue, b: ClarityValue): boolean {
   if (a.type !== b.type) return false;
   return JSON.stringify(a) === JSON.stringify(b);
 }
+
+export function cvCompare(a: ClarityValue, b: ClarityValue): -1 | 0 | 1 {
+  const aStr = JSON.stringify(a);
+  const bStr = JSON.stringify(b);
+  if (aStr < bStr) return -1;
+  if (aStr > bStr) return 1;
+  return 0;
+}
