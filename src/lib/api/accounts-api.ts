ac200d07b91f842eea7ae305dd8b66bc0c8a3b11
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:10:00 +0100

    add export type StxBalance = { balance: string; t to accounts-api

--- src/lib/api/accounts-api.ts
+++ src/lib/api/accounts-api.ts
@@ -7,3 +7,5 @@ export const HIRO_TESTNET_BASE = 'https://api.testnet.hiro.so';
 export type Network = 'mainnet' | 'testnet';
 
 export type AccountBalance = { stx: StxBalance; fungible_tokens: Record<string, FtBalance>; non_fungible_tokens: Record<string, NftHolding> };
+
+export type StxBalance = { balance: string; total_sent: string; total_received: string; total_fees_sent: string; total_miner_rewards_received: string; lock_tx_id: string; locked: string; lock_height: number; burnchain_lock_height: number; burnchain_unlock_height: number };
