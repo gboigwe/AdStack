7b57864613a7165bd7e3d4728b6c9d8c7043a86f
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:12:00 +0100

    add export type TokenTransferPayload = { type: 't to transaction-builder

--- src/lib/transactions/transaction-builder.ts
+++ src/lib/transactions/transaction-builder.ts
@@ -9,3 +9,5 @@ export const TESTNET_VERSION: TransactionVersion = 1;
 export type TransactionType = 'token_transfer' | 'contract_call' | 'smart_contract' | 'coinbase';
 
 export type TransactionPayload = TokenTransferPayload | ContractCallPayload;
+
+export type TokenTransferPayload = { type: 'token_transfer'; recipient: string; amount: bigint; memo: string };
