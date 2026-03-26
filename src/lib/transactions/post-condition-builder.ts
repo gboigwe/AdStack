965f6353fb01709bb61659fbebfc332b23b5ee07
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:20:00 +0100

    add export type NonFungibleConditionCode = 'owns' | 'not_owns' to post-condition-builder

--- src/lib/transactions/post-condition-builder.ts
+++ src/lib/transactions/post-condition-builder.ts
@@ -1,3 +1,5 @@
 // Stacks Transactions post condition builders
 
 export type FungibleConditionCode = 'eq' | 'gt' | 'gte' | 'lt' | 'lte';
+
+export type NonFungibleConditionCode = 'owns' | 'not_owns';
