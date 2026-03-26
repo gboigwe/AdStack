da52993371a020eef4904a204be8af544278c538
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:38:00 +0100

    add export type SignatureData = { signature: to sign-request

--- src/lib/connect/sign-request.ts
+++ src/lib/connect/sign-request.ts
@@ -1,3 +1,5 @@
 // Stacks Connect sign message request builders
 
 export type SignMessageParams = { message: string; onFinish?: (data: SignatureData) => void; onCancel?: () => void };
+
+export type SignatureData = { signature: string; publicKey: string };
