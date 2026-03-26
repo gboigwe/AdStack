3d342eb5f04f5ae4b6ea8176be7606831fa2344f
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:08:00 +0100

    add export type Block = { hash: string; height: number; burn_block_time: number; burn_block_hash: string; miner_txid: string; tx_count: number } to blocks-api

--- src/lib/api/blocks-api.ts
+++ src/lib/api/blocks-api.ts
@@ -1,2 +1,4 @@
 // Hiro API blocks endpoints
 import { HIRO_API_BASE, HIRO_TESTNET_BASE, Network } from './accounts-api';
+
+export type Block = { hash: string; height: number; burn_block_time: number; burn_block_hash: string; miner_txid: string; tx_count: number };
