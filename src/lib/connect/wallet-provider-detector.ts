1ff32b63b888aa8c224a4b3bc87f3b5e4bcd4a96
Gbolahan Akande <geakande@gmail.com>
2026-03-26 12:56:00 +0100

    add WALLET_PROVIDER_VERSION_52 provider version constant

--- src/lib/connect/wallet-provider-detector.ts
+++ src/lib/connect/wallet-provider-detector.ts
@@ -143,3 +143,5 @@ export const WALLET_PROVIDER_VERSION_49 = '49';
 export const WALLET_PROVIDER_VERSION_50 = '50';
 
 export const WALLET_PROVIDER_VERSION_51 = '51';
+
+export const WALLET_PROVIDER_VERSION_52 = '52';
