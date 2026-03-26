77501ad89d98938b97466db0e61e46b92c84e550
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:24:00 +0100

    add export type FtInfo = { name: string; symbol: string; decimals: number; total_supply: string; token_uri: string; contract_id: string } to tokens-api

--- src/lib/api/tokens-api.ts
+++ src/lib/api/tokens-api.ts
@@ -1,2 +1,4 @@
 // Hiro API token endpoints
 import { HIRO_API_BASE, HIRO_TESTNET_BASE, Network } from './accounts-api';
+
+export type FtInfo = { name: string; symbol: string; decimals: number; total_supply: string; token_uri: string; contract_id: string };
