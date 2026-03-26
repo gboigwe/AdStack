ac1279754c79a1f139aa128650a7f601c20bd2dd
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:16:00 +0100

    define SerializedCV output type

--- src/lib/stacksjs/cv-serializer.ts
+++ src/lib/stacksjs/cv-serializer.ts
@@ -1,2 +1,4 @@
 // Stacks.js ClarityValue serialization to hex
 import type { ClarityValue, UintCV, IntCV, BoolCV, NoneCV, BufferCV, StringAsciiCV, StringUtf8CV } from './clarity-value-factory';
+
+export type SerializedCV = { hex: string; type: string };
