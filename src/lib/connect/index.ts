502d26e2ef0c4b2527a419e4b1ba3c8aeb21cae1
Gbolahan Akande <geakande@gmail.com>
2026-03-26 11:12:00 +0100

    add connect module barrel export

---
+++ src/lib/connect/index.ts
@@ -0,0 +1,6 @@
+export * from './auth-request';
+export * from './contract-call-request';
+export * from './stx-transfer-request';
+export * from './wallet-provider-detector';
+export * from './sign-request';
+export * from './WalletContext';
