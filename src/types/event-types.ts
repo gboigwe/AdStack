4a5b58df1386aad8b010e24bd7ce550d326e9e19
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:06:00 +0100

    add BaseContractEvent interface

--- src/types/event-types.ts
+++ src/types/event-types.ts
@@ -1,2 +1,8 @@
 // Stacks smart contract event types
 export type ContractEventType = 'stx_asset' | 'non_fungible_token_asset' | 'fungible_token_asset' | 'data_var' | 'contract_log';
+
+export interface BaseContractEvent {
+  event_index: number;
+  tx_id: string;
+  event_type: ContractEventType;
+}
