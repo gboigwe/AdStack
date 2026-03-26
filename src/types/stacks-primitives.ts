ca0c042fe04f1527ed3ef1889f4ca7d8ec3e7e1c
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:06:00 +0100

    add TxId BlockHash BurnBlockHash branded types

--- src/types/stacks-primitives.ts
+++ src/types/stacks-primitives.ts
@@ -4,3 +4,7 @@ export type Brand<T, B> = T & { [__brand]: B };
 
 export type Uint128 = Brand<bigint, 'Uint128'>;
 export type Int128 = Brand<bigint, 'Int128'>;
+
+export type TxId = Brand<string, 'TxId'>;
+export type BlockHash = Brand<string, 'BlockHash'>;
+export type BurnBlockHash = Brand<string, 'BurnBlockHash'>;
