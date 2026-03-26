51d84d4a3b976e20f8ffa265e1357d685036f499
Gbolahan Akande <geakande@gmail.com>
2026-03-26 18:32:00 +0100

    add TEST_CASE_ID_248 constant

--- src/__tests__/test-constants.ts
+++ src/__tests__/test-constants.ts
@@ -249,3 +249,4 @@ export const TEST_CASE_ID_244 = 244;
 export const TEST_CASE_ID_245 = 245;
 export const TEST_CASE_ID_246 = 246;
 export const TEST_CASE_ID_247 = 247;
+export const TEST_CASE_ID_248 = 248;
