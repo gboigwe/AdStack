059223f70894c5f9a3d795061b3c9af17d6177f2
Gbolahan Akande <geakande@gmail.com>
2026-03-26 18:18:00 +0100

    add HOOK_RETRY_DELAY_205 constant

--- src/hooks/hook-constants.ts
+++ src/hooks/hook-constants.ts
@@ -204,3 +204,4 @@ export const HOOK_RETRY_DELAY_201 = 20100;
 export const HOOK_RETRY_DELAY_202 = 20200;
 export const HOOK_RETRY_DELAY_203 = 20300;
 export const HOOK_RETRY_DELAY_204 = 20400;
+export const HOOK_RETRY_DELAY_205 = 20500;
