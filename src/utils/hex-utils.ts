9d9027bd7721ba472318bae651ae51d9e8911792
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:44:00 +0100

    add bytesToHex converter

--- src/utils/hex-utils.ts
+++ src/utils/hex-utils.ts
@@ -9,3 +9,7 @@ export function hexToBytes(hex: string): Uint8Array {
   }
   return bytes;
 }
+
+export function bytesToHex(bytes: Uint8Array): string {
+  return Array.from(bytes).map(b => b.toString(16).padStart(2, '0')).join('');
+}
