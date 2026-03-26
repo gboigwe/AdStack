21b0fa6ddae961763bf8af88a8c8756265b2b465
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:14:00 +0100

    add export type ContractCallPayload = { type: 'co to transaction-builder

--- src/lib/transactions/transaction-builder.ts
+++ src/lib/transactions/transaction-builder.ts
@@ -11,3 +11,5 @@ export type TransactionType = 'token_transfer' | 'contract_call' | 'smart_contra
 export type TransactionPayload = TokenTransferPayload | ContractCallPayload;
 
 export type TokenTransferPayload = { type: 'token_transfer'; recipient: string; amount: bigint; memo: string };
+
+export type ContractCallPayload = { type: 'contract_call'; contractAddress: string; contractName: string; functionName: string; functionArgs: unknown[] };
