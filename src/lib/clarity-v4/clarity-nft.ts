15cba6a5220b74214925f706d011e57b82de3071
Gbolahan Akande <geakande@gmail.com>
2026-03-26 18:26:00 +0100

    define NftTransferParams type

--- src/lib/clarity-v4/clarity-nft.ts
+++ src/lib/clarity-v4/clarity-nft.ts
@@ -3,3 +3,5 @@
 export type NftAsset = { contractId: string; assetName: string; tokenId: bigint };
 
 export type NftMintParams = { recipient: string; tokenId: bigint; metadata?: string };
+
+export type NftTransferParams = { tokenId: bigint; sender: string; recipient: string };
