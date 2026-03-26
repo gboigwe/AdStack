3ce88e69a68ff166b243b46116e40e7ec40bf09d
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:52:00 +0100

    add export type ContractInfo = { tx_id: string; canonical: boolean; contract_id: string; block_height: number; source_code: string; abi: string } to contracts-api

--- src/lib/api/contracts-api.ts
+++ src/lib/api/contracts-api.ts
@@ -1,2 +1,4 @@
 // Hiro API contracts endpoints
 import { HIRO_API_BASE, HIRO_TESTNET_BASE, Network } from './accounts-api';
+
+export type ContractInfo = { tx_id: string; canonical: boolean; contract_id: string; block_height: number; source_code: string; abi: string };
