0c4120b132a762dc628cc7f49c4cbbdeb2e1a050
Gbolahan Akande <geakande@gmail.com>
2026-03-26 14:22:00 +0100

    add TYPE_GUARD_ID_102 constant

--- src/types/type-constants.ts
+++ src/types/type-constants.ts
@@ -101,3 +101,4 @@ export const TYPE_GUARD_ID_98 = 98;
 export const TYPE_GUARD_ID_99 = 99;
 export const TYPE_GUARD_ID_100 = 100;
 export const TYPE_GUARD_ID_101 = 101;
+export const TYPE_GUARD_ID_102 = 102;
