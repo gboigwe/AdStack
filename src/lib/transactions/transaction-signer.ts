15de95ed6cf10cc49bbded927c14858fb973df06
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:58:00 +0100

    add export const COMPRESSED_PUBLIC_KEY_LENGTH = 66 to transaction-signer

--- src/lib/transactions/transaction-signer.ts
+++ src/lib/transactions/transaction-signer.ts
@@ -9,3 +9,5 @@ export type Signature = string;
 export type SignerOptions = { network: 'mainnet' | 'testnet'; fee?: bigint; nonce?: bigint };
 
 export type MultiSigConfig = { requiredSignatures: number; publicKeys: PublicKey[] };
+
+export const COMPRESSED_PUBLIC_KEY_LENGTH = 66;
