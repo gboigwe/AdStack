f81b08ec8470e26e80c34b1af77f60de33c42d05
Gbolahan Akande <geakande@gmail.com>
2026-03-26 18:28:00 +0100

    define NftBurnParams type

--- src/lib/clarity-v4/clarity-nft.ts
+++ src/lib/clarity-v4/clarity-nft.ts
@@ -5,3 +5,5 @@ export type NftAsset = { contractId: string; assetName: string; tokenId: bigint 
 export type NftMintParams = { recipient: string; tokenId: bigint; metadata?: string };
 
 export type NftTransferParams = { tokenId: bigint; sender: string; recipient: string };
+
+export type NftBurnParams = { tokenId: bigint; owner: string };
