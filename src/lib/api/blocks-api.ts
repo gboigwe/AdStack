1e98901eb2693d8acedd689b492b937ea46ba44f
Gbolahan Akande <geakande@gmail.com>
2026-03-26 10:12:00 +0100

    add export type BurnBlock = { burn_block_hash: string; burn_block_height: number; stacks_blocks: string[] } to blocks-api

--- src/lib/api/blocks-api.ts
+++ src/lib/api/blocks-api.ts
@@ -4,3 +4,5 @@ import { HIRO_API_BASE, HIRO_TESTNET_BASE, Network } from './accounts-api';
 export type Block = { hash: string; height: number; burn_block_time: number; burn_block_hash: string; miner_txid: string; tx_count: number };
 
 export type BlocksResponse = { limit: number; offset: number; total: number; results: Block[] };
+
+export type BurnBlock = { burn_block_hash: string; burn_block_height: number; stacks_blocks: string[] };
