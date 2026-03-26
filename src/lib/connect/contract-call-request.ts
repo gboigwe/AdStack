91419245f9a99074663d6b793a6b3ac866fdd52e
Gbolahan Akande <geakande@gmail.com>
2026-03-26 09:42:00 +0100

    add export type TxBroadcastResult = { txId:  to contract-call-request

--- src/lib/connect/contract-call-request.ts
+++ src/lib/connect/contract-call-request.ts
@@ -7,3 +7,5 @@ export type PostConditionMode = 'allow' | 'deny';
 export type AnchorMode = 'any' | 'on_chain_only' | 'off_chain_only';
 
 export type ContractCallRequestParams = { contractAddress: string; contractName: string; functionName: string; functionArgs: unknown[]; network: Network; postConditions?: unknown[]; postConditionMode?: PostConditionMode; anchorMode?: AnchorMode; fee?: bigint; nonce?: bigint; onFinish?: (data: TxBroadcastResult) => void; onCancel?: () => void };
+
+export type TxBroadcastResult = { txId: string; txRaw: string };
